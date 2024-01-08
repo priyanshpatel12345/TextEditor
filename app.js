6f5c3dc4efeb442fbee46ad0bb58fcc005810487
Priyansh Patel <145756010+priyanshpatel12345@users.noreply.github.com>
2024-01-08 09:30:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let inp = document.querySelector("#input");
+
+let p = document.querySelector("p");
+
+inp.addEventListener("input", function() {
+    console.log(this.value);
+    p.innerText = this.value;
+
+});
+
